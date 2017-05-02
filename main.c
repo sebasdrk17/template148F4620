3016fadb56cc751e077e5d17f0ba1ab169428e1d
Marcos Puente <jmpuente@gmail.com>
2017-05-02 17:28:08 -0500

    Fix al template

--- main.c
+++ main.c
@@ -1,11 +1,10 @@
 #include <18F4620.h>
-#fuses INTRC_IO, NOFCMEN, NOIESO, PUT, NOBROWNOUT, NOWDT
+#fuses HS, NOFCMEN, NOIESO, PUT, NOBROWNOUT, NOWDT
 #fuses NOPBADEN, NOMCLR, STVREN, NOLVP, NODEBUG
-#use delay(clock=32000000)
+#use delay(clock=16000000)
 
 void main (void){
-   setup_oscillator(OSC_32MHZ);
-   set_tris_D(0x00);
+   setup_oscillator(OSC_16MHZ);
    while(1){
       
    }
