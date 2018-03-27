358f5cc2dde7b6954d36bc2ccae418df08df0556
arkan <jmpuente@gmail.com>
2018-03-26 20:32:30 -0600

    Agregamos Debugger por serial
    * Se agrega la interfaz serial para usarla como método de depuración
    * El pin no se puede usar para nada mas que envío de datos seriales

--- main.c
+++ main.c
@@ -3,8 +3,13 @@
 #fuses NOPBADEN, NOMCLR, STVREN, NOLVP, NODEBUG
 #use delay(clock=16000000)
 
+#define TX_232        PIN_C6
+#use RS232(BAUD=9600, XMIT=TX_232, BITS=8,PARITY=N, STOP=1)
+#use fast_io(c)
+
 void main (void){
    setup_oscillator(OSC_16MHZ);
+   printf("Hola Mundo\n");//Puedes usar putc o printf. Revisa la documentación de CCS para ver que mas puedes hacer.
    while(1){
       
    }
